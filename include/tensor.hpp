a95713f790971b27d0e2e1d6d16a1fa4dd5d874e
xgui <xgui@pinterest.com>
2025-04-30 07:05:19 +0000

    basic structure

---
+++ include/tensor.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+#include <vector>
+#include <cuda_runtime.h>
+
+enum class DeviceType {
+    HOST,
+    DEVICE
+};
+
+template <typename T>
+class Tensor {
+public:
+    Tensor(const std::vector<size_t>& shape, DeviceType device = DeviceType::HOST);
+    Tensor(const Tensor&) = delete;
+    Tensor& operator=(const Tensor&) = delete;
+    ~Tensor();
+
+    const std::vector<size_t>& shape() const { return shape_; }
+    DeviceType device() const { return device_; }
+    T* data() { return static_cast<T*>(data_); }
+    void copy_from(const Tensor& other);
+    const T* data() const { return static_cast<const T*>(data_); }
+
+private:
+    std::vector<size_t> shape_;
+    size_t size_;
+    DeviceType device_;
+    void* data_;
+};
+
+
+template<typename T>
+Tensor<T>::Tensor(const std::vector<size_t>& shape, DeviceType device): shape_(shape), device_(device) {
+    size_ = 1;
+    for (auto d : shape_) {
+        size_ *= d;
+    }
+    if (device_ == DeviceType::HOST) {
+        data_ = (void*)new T[size_];
+    } else {
+        cudaError_t err = cudaMalloc(&data_, sizeof(T) * size_);
+        if (err != cudaSuccess) {
+            throw std::runtime_error("cudaMalloc failed!");
+        }
+    }
+}
+
+template<typename T>
+Tensor<T>::~Tensor() {
+    if (device_ == DeviceType::HOST) {
+        delete[] (static_cast<T*>(data_));
+    } else {
+        cudaFree(data_);
+    }
+}
+
+template<typename T>
+void Tensor<T>::copy_from(const Tensor& other) {
+    if (other.size_ != size_ || other.device_ == device_)
+        throw std::runtime_error("Copy from: shape or device mismatch");
+
+    if (device_ == DeviceType::HOST && other.device_ == DeviceType::DEVICE) {
+        cudaMemcpy(data_, other.data_, size_ * sizeof(T), cudaMemcpyDeviceToHost);
+    }
+    else if (device_ == DeviceType::DEVICE && other.device_ == DeviceType::HOST) {
+        cudaMemcpy(data_, other.data_, size_ * sizeof(T), cudaMemcpyHostToDevice);
+    }
+    else {
+        throw std::runtime_error("Unsupported copy direction");
+    }
+}
---
+++ src/kernels/gemm.cu
@@ -0,0 +1,47 @@
+#include <cstddef>
+#include <cuda_runtime.h>
+#include "kernels/gemm.h"
+
+/**
+    s1: [batch_size, rows, N]
+    s2: [batch_size, N, cols]
+    output: [batch_size, rows, cols]
+*/
+template<typename T>
+__global__ void gemm_kernel(const T* s1, const T* s2, T* output, size_t batch_size, size_t rows, size_t N, size_t cols) {
+    __shared__ T s1_shared[TILE_SIZE][TILE_SIZE];
+    __shared__ T s2_shared[TILE_SIZE][TILE_SIZE];
+    const T* base_s1 = s1 + (blockIdx.z * rows * N);
+    const T* base_s2 = s2 + (blockIdx.z * N * cols);
+    T result = 0;
+    for (size_t i = 0; i < N; i += TILE_SIZE) {
+        if ((blockIdx.y * TILE_SIZE + threadIdx.y) < rows && (i + threadIdx.x) < N) {
+            s1_shared[threadIdx.y][threadIdx.x] = base_s1[(blockIdx.y * TILE_SIZE + threadIdx.y) * N + i + threadIdx.x];
+        } else {
+            s1_shared[threadIdx.y][threadIdx.x] = 0;
+        }
+
+        if ((blockIdx.x * TILE_SIZE + threadIdx.x) < cols && (i + threadIdx.y) < N) {
+            s2_shared[threadIdx.y][threadIdx.x] = base_s2[(i + threadIdx.y) * cols + blockIdx.x * TILE_SIZE + threadIdx.x];
+        } else {
+            s2_shared[threadIdx.y][threadIdx.x] = 0;
+        }
+        __syncthreads();
+        for (size_t j = 0; j < TILE_SIZE; ++j) {
+            result += (s1_shared[threadIdx.y][j] * s2_shared[j][threadIdx.x]);
+        }
+        __syncthreads();
+    }
+    output[blockIdx.z * rows * cols + (blockIdx.y * TILE_SIZE + threadIdx.y) * cols + blockIdx.x * TILE_SIZE + threadIdx.x] = result;
+}
+
+template<typename T>
+void launch_gemm_kernel(const T* s1, const T* s2, T* output, size_t batch_size, size_t rows, size_t N, size_t cols) {
+    dim3 blockDim(TILE_SIZE, TILE_SIZE);
+    dim3 gridDim((cols + TILE_SIZE - 1) / TILE_SIZE, (rows + TILE_SIZE - 1) / TILE_SIZE, batch_size);
+    gemm_kernel<<<gridDim, blockDim>>>(s1, s2, output, batch_size, rows, N, cols);
+}
+
+// Explicit template instantiation
+template void launch_gemm_kernel<float>(const float*, const float*, float*, 
+    size_t, size_t, size_t, size_t);
