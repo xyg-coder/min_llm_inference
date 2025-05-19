bbe022ca2f03c598cd8c5ac52c666e9ef120eb43
xgui <xgui@pinterest.com>
2025-05-19 06:02:38 +0000

    more designs about the pipeline

--- include/items_storage.h
+++ include/items_storage.h
@@ -29,11 +29,12 @@ public:
     // return at most size new items
     std::vector<IdStringPair> get_new_items(int size);
     void add_one_finished_item(const IdStringPair&);
+    bool is_finished() const;
 
     /**
      * assert decoder_result.size() == processing_items_.size()
      * 1. for each processing_items_, append decoder_result to the end
-     * 2. check the length of processing_items_ and if meets the ending token
+     * 2. check the length of processing_items_ or if meets the ending token or special_token(-1 means this is empty item)
      * 3. insert those finished items to finished_items_
      * 4. returns the indices ([0, n_batch-1]) of the finished results
      */
@@ -61,9 +62,9 @@ std::vector<int> process_decoder_result(
 /**
  * 1. fetch new_items with finished_indices.size()
  * 2. fill new_tokens and new_lengths. Note, we have to fill n_finished_items even there isn't enough new_items. But padding it with length==0
- * 3. call launch_inference_optimized_encoder_kernel
- * After this step, we are ready to call self-attention block
+ * After this step, we are ready to call encoder
  */
 void insert_new_items(
-    const std::vector<int>& finished_indices
-);
+    const std::vector<int>& finished_indices, TensorInt& new_items, TensorInt& new_lengths,
+    TensorInt& new_items_indices);
+    
